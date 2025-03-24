856b0480973e7f03dc0447f546783dccb38b812a
MistoWest <mister.west529@gmail.com>
2025-03-23 23:24:34 -0300

    dev

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,4 @@
-"use strict";
-console.warn("Hello World");
+import { world, system} from "@minecraft/server"
+import { ActionFormData, ModalFormData} from "@minecraft/server-ui"
+
+console.warn("Ok")
